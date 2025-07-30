448869ad38c7bc15e86836f9ec1cbd29706ac6b4
MONOLIFESTUDIO <higorcosta188@gmail.com>
2025-07-30 19:15:27 -0300

    Update config.js

--- config.js
+++ config.js
@@ -109,7 +109,9 @@ const TIMEZONE = 'America/Sao_Paulo';
  * const PRELOADER_LOGO_URL = 'https://dropbox.com/s/abc/logo.png?dl=0';
  * const PRELOADER_LOGO_URL = ''; // Sem logo
  */
-const PRELOADER_LOGO_URL = 'https://drive.google.com/file/d/1r-m9txwTIFxbY5S4QysZTkEvbYTrvbcY/view'; // Adicione a URL do seu logo aqui
+const PRELOADER_LOGO_URL = '';
+// const PRELOADER_LOGO_URL = 'https://drive.google.com/file/d/1r-m9txwTIFxbY5S4QysZTkEvbYTrvbcY/view'; // Adicione a URL do seu logo aqui
+
 
 /**
  * COR DO PRELOADER:
